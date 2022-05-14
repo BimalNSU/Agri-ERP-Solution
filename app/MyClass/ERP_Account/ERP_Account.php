184e3409cf13f5bb6870134563b18b99f9580272
BimalNSU <bimal.nsu.cse@gmail.com>
2022-05-14 20:05:23 +0600

    all functions r implemented in 'ERP_Account' class

--- app/MyClass/ERP_Account/ERP_Account.php
+++ app/MyClass/ERP_Account/ERP_Account.php
@@ -1,5 +1,5 @@
 <?php
-namespace App\MyClass;
+namespace App\MyClass\ERP_Account;
 use Illuminate\Support\Facades\DB;
 
 class ERP_Account {
@@ -7,32 +7,60 @@ class ERP_Account {
     {
 
     }
-    function updateAccountStatus($erp_account_id, $is_active)
+    function updateAccountStatus($erpAccountId, $isActive)
     {
-
+        $query = " 
+            UPDATE ERP_accounts
+                SET is_active = ?
+            WHERE id = ?";
+        $result = DB::update($query, [$isActive, $erpAccountId]);
+        $result;
     }
-    function addBalance($erp_account_id, $amount)
+    function addBalance($erpAccountId, $amount)
     {
-
+        $query = " 
+            UPDATE ERP_accounts
+                SET balance = balance + ?
+            WHERE id = ?";
+        $result = DB::update($query, [$amount, $erpAccountId]);
     }
-    function useBalance($erp_account_id, $use_amount)
+    function useBalance($erpAccountId, $useAmount)
     {
-
+        $query = " 
+            UPDATE ERP_accounts
+                SET balance = balance - ?
+            WHERE id = ?";
+        $result = DB::update($query, [$useAmount, $erpAccountId]);
+        return $result;
     }
-    function getAssignedAccountantList($erp_account_id)
+    function getAssignedAccountantList($erpAccountId)
     {
-        // [
-        //     {
-        //       accountant_id: int,
-        //       name: string,
-        //       mobile: string
-        //     },
-        //   ]
+        $query = " 
+            SELECT y.accountant_id, concat(u1.first_name, ' ', u1.last_name) name, u1.mobile
+            FROM ERP_accounts x JOIN ERP_accountants y
+                    ON x.id = y.erp_account_id AND
+                        x.id = ?
+                    JOIN Users u1
+                    ON y.accountant_id = u1.id";
+        $result = DB::select($query, [$erpAccountId]);
+        return json_decode(json_encode($result), true);
     }
     
-    function getAccountType($erp_account_id)
+    function getAccountType($erpAccountId)
     {
-        return '';
+        $query = " 
+            SELECT case
+                        WHEN ea.id IS NULL then 'None'
+                        WHEN ea.id IS NOT NULL AND
+                                ebd.erp_account_id IS NULL then 'Offline'
+                        ELSE 'Online'
+                    END 'type'
+            FROM ERP_accounts ea LEFT JOIN ERP_bank_details ebd
+                    ON ea.id = ebd.erp_account_id
+            WHERE ea.id = ?";
+        $result = DB::select($query, [$erpAccountId]);
+        $result = json_decode($result, true);
+        return $result['type'];
     }
 }
 ?>
