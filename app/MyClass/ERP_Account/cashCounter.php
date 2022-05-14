b40c53bcc76b50d8fd55cf18d95617929e5d2fa2
BimalNSU <bimal.nsu.cse@gmail.com>
2022-05-14 20:13:27 +0600

    all functions r implemented in 'CashCounter' class

--- app/MyClass/ERP_Account/cashCounter.php
+++ app/MyClass/ERP_Account/cashCounter.php
@@ -7,5 +7,31 @@ class CashCounter {
     {
 
     }
+    public createAccount($cashCounterName, $balance, $isActive)
+    {
+        $query = " 
+            INSERT INTO ERP_accounts (name, balance, is_active)
+                VALUES(?, ?, ?)";
+        $result = DB::insert($query, [$cashCounterName, $balance, $isActive]);
+    }                
+    public function getCounterList()
+    {
+        $query = "
+            SELECT id cash_counter_id, name couter_name
+            FROM ERP_accounts
+            WHERE id NOT IN (SELECT erp_account_id
+                            FROM ERP_bank_details)";
+        $result = DB::select($query);
+        return json_decode(json_encode($result), true);
+    }
+    public function getCounterBalanceList()
+    {
+        $query = "
+            SELECT id cash_counter_id, name couter_name, balance
+            FROM ERP_accounts
+            WHERE id NOT IN (SELECT erp_account_id FROM ERP_bank_details)";
+        $result = DB::select($query);
+        return json_decode(json_encode($result), true);
+    }
 }
 ?>
